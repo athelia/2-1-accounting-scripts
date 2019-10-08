22821a42f0ca0668e71cb76f5adbe170dfbf7e1a
Athelia Crosmun <athelia.hk@gmail.com>
2019-10-08 16:25:33 +0000

    Fixed comma separations and strings

--- melons.py
+++ melons.py
@@ -1,42 +1,48 @@
+"""Melon data"""
+
 melons = {
     'Honeydew': {
-    price: 0.99
-    seedless: True
-    flesh_color: None
-    rind_color: None
-    weight: None
-    origin: None
-    }
+        'price': 0.99,
+        'seedless': True,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+        'origin': None
+    },
+
     'Crenshaw': {
-    price: 2.00
-    seedless: False
-    flesh_color: None
-    rind_color: None
-    weight: None
-    origin: None
-    }
+        'price': 2.00,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+        'origin': None
+    },
+
     'Crane': {
-    price: 2.50
-    seedless: False
-    flesh_color: None
-    rind_color: None
-    weight: None
-    origin: None
-    }
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+        'origin': None
+    },
+
     'Casaba': {
-    price: 2.50
-    seedless: False
-    flesh_color: None
-    rind_color: None
-    weight: None
-    origin: None
-    }
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+        'origin': None
+    },
+    
     'Cantaloupe': {
-    price: 0.99
-    seedless: False
-    flesh_color: None
-    rind_color: None
-    weight: None
-    origin: None
+        'price': 0.99,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+        'origin': None
     }
 }
